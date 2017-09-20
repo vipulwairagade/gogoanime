b1613d69634416e19f2bd1926acd6bb958ee4147
Vipul Wairagade <sonu.wairagade@gmail.com>
2017-09-21 00:27:54 +0530

    Update anime.py

--- anime.py
+++ anime.py
@@ -1,37 +1,29 @@
 import requests
 from bs4 import *
 
-animelink = raw_input("Enter Link of your Anime Series : ")
+animelink = raw_input("Enter Link of your Anime Series : ")  #User Enters URL
 
 start = int(input("Enter Episode Number to start with : "))
 end = int(input("Enter Episode Number to end with : "))
 print "Generating Links from", start, "to", end 
-end=end+1
+end=end+1 # Increased by 1 for range function
 
-#https://ww3.gogoanime.io/category/boruto-naruto-next-generations
-animename = animelink.split("/")
-
-
-links = []
-URL_PATTERN = 'https://ww3.gogoanime.io/{}-episode-{}'
+animename = animelink.split("/")  #splits link by /
+URL_PATTERN = 'https://ww3.gogoanime.io/{}-episode-{}' #General URL pattern for every anime on gogoanime
 for episode in range(start,end):
 	url = URL_PATTERN.format(animename[4],episode)
 	srcCode = requests.get(url)
 	plainText = srcCode.text
 	soup = BeautifulSoup(plainText,"lxml")
-	source_url = soup.find('div', {'class': 'download-anime'}).find('a')     #get all elements with 'a' tag
+	source_url = soup.find('div', {'class': 'download-anime'}).find('a')     #get element with 'a' tag
 	link = source_url.get('href')
-	links.append(link)
-	print link
-	
-for dowurl in links:
-	dowCode = requests.get(dowurl)
+	dowCode = requests.get(link)
 	data = dowCode.text
 	soup = BeautifulSoup(data,"lxml")
-	dow_url = soup.findAll('div', {'class': 'dowload'})[2].find('a')     #get all elements with 'a' tag
+	dow_url = soup.findAll('div', {'class': 'dowload'})[2].find('a')     #get 3rd elements with 'a' tag
 	dowlink = dow_url.get('href')
 	print dowlink
-	f = open("dowlink.txt","a") #opens file with name of "test.txt"
+	f = open('{}.txt'.format(animename[4]),"a") #opens file with name of "test.txt"
 	f.write(dowlink+"\n\n")
 
 f.close()
